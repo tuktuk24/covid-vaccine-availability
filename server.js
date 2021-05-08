e97c6ad7efb36e44891bacb46dcf0488e135f9e1
tuktuk24 <gotuktuk24@gmail.com>
2021-05-08 21:07:22 +0530

    Happy Path

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const {callApi} =  require('./functions')
 const app = express()
 const port = process.env.PORT || 5002
 app.get('/',async (req, res) => {
-    const data = await callApi('784001', '08-05-2021')
-    res.send(data)
+    // const data = await callApi('784001', '08-05-2021')
+    res.send('Hello World')
 })
 
 app.listen(process.env.PORT, () => {
