6ab259fda35f30e846db7a2ef31d516ad15c9492
tuktuk24 <gotuktuk24@gmail.com>
2021-05-10 11:04:45 +0530

    debugging1

--- functions.js
+++ functions.js
@@ -35,7 +35,8 @@ const callApi = async (pincode, date) => {
                 method: 'GET',
                 headers: {
                     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36",
-                    'Accept': '*/*'
+                    'Accept': '*/*',
+                    'Content-Type': 'application/json'
                 }
             }
         )
