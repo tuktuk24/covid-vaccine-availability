bd1e480489f7a517781e58d55965a682ec944c7e
tuktuk24 <gotuktuk24@gmail.com>
2021-05-10 10:56:24 +0530

    error handling

--- functions.js
+++ functions.js
@@ -9,30 +9,40 @@ const SEARCH_BY_PINCODE = '/v2/appointment/sessions/public/findByPin'
 
 
 const getStateData = async () => {
-    const response = await fetch(BASE_URL + STATE_DATA,
-        {
-            method: 'GET',
-            headers: {
-                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36",
-                'Accept': '*/*'
+    let data = undefined
+    try {
+        const response = await fetch(BASE_URL + STATE_DATA,
+            {
+                method: 'GET',
+                headers: {
+                    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36",
+                    'Accept': '*/*'
+                }
             }
-        }
-    )
-    const data = await response.json()
+        )
+        const data = await response.json()
+    } catch (error) {
+        data = error
+    }
     return data
 }
 
 const callApi = async (pincode, date) => {
-    const response = await fetch(`${BASE_URL}${SEARCH_BY_PINCODE}?Accept-Language=en_US&pincode=${pincode}&date=${date}`,
-        {
-            method: 'GET',
-            headers: {
-                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36",
-                'Accept': '*/*'
+    let data = undefined
+    try {
+        const response = await fetch(`${BASE_URL}${SEARCH_BY_PINCODE}?Accept-Language=en_US&pincode=${pincode}&date=${date}`,
+            {
+                method: 'GET',
+                headers: {
+                    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36",
+                    'Accept': '*/*'
+                }
             }
-        }
-    )
-    const data = await response.json()
+        )
+        data = await response.json()
+    } catch (error) {
+        data = error
+    }
     return data
 }
 
