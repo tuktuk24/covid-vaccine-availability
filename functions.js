f8d61dee2e3b6cd5eccae6a67fa848e68ca55f03
tuktuk24 <gotuktuk24@gmail.com>
2021-05-10 11:21:29 +0530

    debugging4

--- functions.js
+++ functions.js
@@ -34,7 +34,7 @@ const callApi = async (pincode, date) => {
             {
                 method: 'GET',
                 headers: {
-                    "Accept-Language": "en_US",
+                    "Accept-Language": "hi_IN",
                     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36",
                     'Accept': '*/*',
                     'Content-Type': 'application/json'
